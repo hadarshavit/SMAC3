40378dd0bf32fad6898391b8806615e75ed15864
Hadar Shavit <shavit@aim.rwth-aachen.de>
2024-10-10 17:05:30 +0200

    Noisy surrogate

--- smac/model/__init__.py
+++ smac/model/__init__.py
@@ -1,6 +1,7 @@
 from smac.model.abstract_model import AbstractModel
 from smac.model.multi_objective_model import MultiObjectiveModel
 from smac.model.random_model import RandomModel
+from smac.model.noisy_surrogate_model import NoisySurrogateModel
 
 __all__ = [
     "AbstractModel",
